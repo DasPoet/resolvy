f2bcfe84aae89644ad79e7dabcf6144ad054597c
Daspoet <cederk2306@gmail.com>
2021-10-23 19:37:59 +0200

    docs: add example for Godoc

---
+++ resolvy_test.go
@@ -0,0 +1,28 @@
+package resolvy
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+func Example() {
+	type Message struct {
+		CreatedAt time.Time   `resolvy:"createdAt,omitempty"`
+		Data      interface{} `resolvy:"data,omitempty"`
+	}
+	msg := Message{CreatedAt: time.Now()}
+
+	data, err := MarshalJSON(msg, MarshalConfig{
+		Marshalers: map[string]FieldMarshaler{
+			"createdAt": func() (interface{}, error) {
+				return msg.CreatedAt.String(), nil
+			},
+		},
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(string(data))
+}
