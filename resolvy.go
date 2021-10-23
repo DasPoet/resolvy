6e603772b990f604729a21e2ed1752038887941e
Daspoet <cederk2306@gmail.com>
2021-10-23 19:32:29 +0200

    docs: add Godoc to MarshalJSON

--- resolvy.go
+++ resolvy.go
@@ -7,6 +7,8 @@ import (
 // TagName is the default tag name used by resolvy.
 const TagName = "resolvy"
 
+// MarshalJSON is a utility for producing JSON
+// from the marshalled representation of v.
 func MarshalJSON(v interface{}, config MarshalConfig) ([]byte, error) {
 	res, err := Marshal(v, config)
 	if err != nil {
